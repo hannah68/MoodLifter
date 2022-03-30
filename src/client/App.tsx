82b8c17af63d3a3c934c763a6abbf8594bd554c5
hannah68 <reihaneh.naderi1989@gmail.com>
2022-03-30 09:37:26 +0100

    create client routes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Feeling from "./pages/Feeling"
+import Profile from "./pages/Profile";
+import Recommendation from "./pages/Recommendation";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
+export interface IAppProps {}
 
-function App() {
-  return (
-    <div>
-      
-    </div>
+const App: React.FunctionComponent<IAppProps> = (props) => {
+	return (
+    <>
+      <div>Hello</div>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/feeling" element={<Feeling/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/recommendation" element={<Recommendation/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+       </Routes>
+    </>
+  
   );
-}
+};
+
 
 export default App;
