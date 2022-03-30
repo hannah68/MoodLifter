fcfbfa69668b5046522130486112384f524b76a0
hannah68 <reihaneh.naderi1989@gmail.com>
2022-03-30 11:47:20 +0100

    build UI for signin form

--- src/client/pages/Signin.tsx
+++ src/client/pages/Signin.tsx
@@ -1,15 +1,32 @@
 import React from "react";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+
+import "../styles/signup.css";
 
 export interface ISigninProps {}
 
 const Signin: React.FunctionComponent<ISigninProps> = (props) => {
 	return (
-    <>
-      <div>Signin page</div>
-    </>
-  
-  );
+		<div className="signin-page">
+			<h1>Sign In</h1>
+			<form className="signin-form">
+				<div className="input-groups">
+					<label htmlFor="email">
+						<MdEmail />
+					</label>
+					<input type="text" placeholder="Email" id="email" />
+				</div>
+				<div className="input-groups">
+					<label htmlFor="password">
+						<RiLockPasswordFill />
+					</label>
+					<input type="text" placeholder="Password" id="password" />
+				</div>
+        <button type="submit" className="register-btn login">Sign In</button>
+			</form>
+		</div>
+	);
 };
-
 
 export default Signin;
