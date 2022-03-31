e34418401e9a7a46de869e364c7a43695fdc22c0
hannah68 <reihaneh.naderi1989@gmail.com>
2022-03-31 16:12:26 +0100

    create data for my database

---
+++ src/server/data/quote.ts
@@ -0,0 +1,115 @@
+import { Quote } from "../config/interfaces";
+
+export const quotes: Array<Quote> = [
+	{
+		text: "Life is like a piano: the white keys represent happiness, the black keys show sadness. But as you go through life’s journey, remember that the black keys make music too.",
+		author: "N/A",
+		quoteType: "Sad",
+	},
+	{
+		text: "Life is a circle of happiness, sadness, hard times, and good times. If you are going through hard times, have faith that good times are on the way.",
+		author: "N/A",
+		quoteType: "Sad",
+	},
+	{
+		text: "There are moments when I wish I could roll back the clock and take all the sadness away, but I have the feeling that if I did, the joy would be gone as well.",
+		author: "Nicholas Sparks",
+		quoteType: "Sad",
+	},
+	{
+		text: "The more powerful and original a mind, the more it will incline towards the religion of solitude.",
+		author: "Aldous Huxley",
+		quoteType: "Lonely",
+	},
+	{
+		text: "Remember: the time you feel lonely is the time you most need to be by yourself. Life’s cruelest irony.",
+		author: "Douglas Coupland",
+		quoteType: "Lonely",
+	},
+	{
+		text: "It’s far better to be unhappy alone than unhappy with someone",
+		author: "Marilyn Monroe",
+		quoteType: "Lonely",
+	},
+    {
+        text: "There is only one way to happiness and that is to cease worrying about things which are beyond the power of our will.",
+		author: "Epictetus",
+		quoteType: "Worried",
+    },
+    {
+        text: "No need to hurry. No need to sparkle. No need to be anybody but oneself.",
+		author: "Virginia Woolf",
+		quoteType: "Worried",
+    },
+    {
+        text: "Anxiety does not empty tomorrow of its sorrows, but only empties today of its strength.",
+		author: "Charles Spurgeon",
+		quoteType: "Worried",
+    },
+    {
+        text: "Decide that you want it more than you are afraid of it.",
+		author: "Bill Cosby",
+		quoteType: "Scared",
+    },
+    {
+        text:"The brave man is not he who does not feel afraid, but he who conquers that fear.",
+		author: "Nelson Mandela",
+		quoteType: "Scared",
+    },
+    {
+        text:"Do you think miners stand around all day talking about how hard it is to mine for coal? They do not. They simply dig.",
+		author: "Cheryl Strayed",
+		quoteType: "Tired",
+    },
+    {
+        text:"Courage is not having the strength to go on; it is going on when you don't have the strength.",
+		author: "Theodore Roosevelt",
+		quoteType: "Tired",
+    },
+    {
+        text:"Anger makes you smaller, while forgiveness forces you to grow beyond what you were.",
+		author: "Chérie Carter-Scott",
+		quoteType: "Angry",
+    },
+    {
+        text:"There are two things a person should never be angry at, what they can help, and what they cannot.",
+		author: "Plato",
+		quoteType: "Angry",
+    },
+    {
+        text:"There are two things a person should never be angry at, what they can help, and what they cannot.",
+		author: "Plato",
+		quoteType: "Angry",
+    },
+    {
+        text:"Boredom always precedes a period of great creativity.",
+		author: "N/A",
+		quoteType: "Bored",
+    },
+    {
+        text:"In order to live free and happily, you must sacrifice boredom. It is not always an easy sacrifice.",
+		author: "Richard Bach",
+		quoteType: "Bored",
+    },
+    {
+        text:"The most confused we ever get is when we're trying to convince our heads of something our heart knows is a lie.",
+		author: "Karen Marie Moning",
+		quoteType: "Confused",
+    },
+    {
+        text:"Good. Be confused. Confusion is where inspiration comes from.",
+		author: "Robyn Mundell",
+		quoteType: "Confused",
+    },
+    {
+        text:"Don’t let insecure thoughts ruin something amazing.",
+		author: "N/A",
+		quoteType: "Insecure",
+    },
+    {
+        text:"No one is perfect. Even the most confident people have insecurities. At some point in of our lives, we may feel we lack something. That is reality. We must try to live as per our capability.",
+		author: "Anil Sinha",
+		quoteType: "Insecure",
+    }
+
+];
---
+++ src/server/data/video.ts
@@ -0,0 +1,72 @@
+import { Video } from "../config/interfaces";
+
+export const Videos: Array<Video> = [
+	{
+		title: "6 Differences Between Sadness and Depression",
+		link: "https://www.youtube.com/watch?v=tNwRNmFT7-4&t=34s",
+		content:
+			"sadness is more of an emotional reaction and generally short term lasting no more than a few days. In this video, Psych2Go shares 6 important distinctions to help you understand the difference between sadness and depression. Share this video with those who could benefit. ",
+		videoType: "Sad",
+	},
+	{
+		title: "The Simple Cure for Loneliness | Baya Voce | TEDxSaltLakeCity",
+		link: "https://www.youtube.com/watch?v=KSXh1YfNyVA&t=77s",
+		content:
+			" Baya reveals a simple tool you can start using today to create more happiness, fulfillment, and connection.",
+		videoType: "Lonely",
+	},
+	{
+		title: "How to cope with anxiety | Olivia Remes | TEDxUHasselt",
+		link: "https://www.youtube.com/watch?v=WWloIAQpMcQ",
+		content:
+			" Olivia Remes of the University of Cambridge will share her vision on anxiety and will unravel ways to treat and manage this health disorder. Arguing that treatments such as psychotherapy and medication exist and often result in poor outcome and high rates of relapses, she will emphasise the importance of harnessing strength in ourselves as we modify our problem-coping mechanisms.",
+		videoType: "Worried",
+	},
+	{
+		title:
+			"Why you should define your fears instead of your goals | Tim Ferriss",
+		link: "https://www.youtube.com/watch?v=5J6jAC6XxAI&t=612s",
+		content:
+			"Tim Ferriss encourages us to fully envision and write down our fears in detail, in a simple but powerful exercise he calls 'fear-setting'.",
+		videoType: "Scared",
+	},
+	{
+		title:
+			"The real reason why we are tired and what to do about it | Saundra Dalton-Smith | TEDxAtlanta",
+		link: "https://www.youtube.com/watch?v=ZGNN4EPJzGk",
+		content:
+			"Saundra Dalton-Smith reveals the real reason why we are chronically tired despite getting the requisite amount of sleep and the 6 other types of rest available to us in this fast-paced and informative talk.",
+		videoType: "Tired",
+	},
+	{
+		title:
+			"Anger Is Your Ally: A Mindful Approach to Anger | Juna Mustad | TEDxWabashCollege",
+		link: "https://www.youtube.com/watch?v=sbVBsrNnBy8",
+		content:
+			" In this pioneering talk, Juna Mustad reveals how neuroscience and mindfulness techniques can help us unlock the power of the world's most stigmatized emotion, anger.",
+		videoType: "Angry",
+	},
+	{
+		title:
+			"Saving Yourself the Cost of Insecurity | Chantelle Anderson | TEDxVanderbiltUniversity",
+		link: "https://www.youtube.com/watch?v=ts9GazDNkuY",
+		content:
+			" In this talk, Confidence Coach Chantelle Anderson takes you on a journey starting with her insecurity – from skipping her first speech to an identity crisis after retiring from the WNBA – and ending in confidently going after her dream career post-basketball.",
+		videoType: "Insecure",
+	},
+	{
+		title: "From Confusion to Conclusion | Swapna Patker | TEDxIIMRohtak",
+		link: "https://www.youtube.com/watch?v=6LZ7QqoY_1w",
+		content:
+			"Life made me do many things that I did not want to do , but those were the things that got me close to what I wanted . My aim in life is exploring all that excites me and I'm doing exactly that . I'm going to speak about my journey from an introvert engineer to an extrovert psychologist. I would also like to share a few funny memories on my educational path.",
+		videoType: "Confused",
+	},
+	{
+		title:
+			"How boredom can lead to your most brilliant ideas | Manoush Zomorodi",
+		link: "https://www.youtube.com/watch?v=c73Q8oQmwzo",
+		content:
+			"Do you sometimes have your most creative ideas while folding laundry, washing dishes or doing nothing in particular? It's because when your body goes on autopilot, your brain gets busy forming new neural connections that connect ideas and solve problems. Learn to love being bored as Manoush Zomorodi explains the connection between spacing out and creativity.",
+		videoType: "Bored",
+	},
+];
