78510a328dda8a6e4c8fed372be08391f3bc5184
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-04 12:13:06 +0100

    create answer functionality

--- src/client/components/FeelingQuestions.tsx
+++ src/client/components/FeelingQuestions.tsx
@@ -1,36 +1,140 @@
-import { FormEvent } from "react";
-import { questions, questionType } from "../data/feelingQuestion";
-
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+// import { questions, questionType } from "../data/feelingQuestion";
 
 export type ShowFeeling = true | false;
 
 interface FeelingQProps {
-    setShowFeelings: (target: ShowFeeling) => void
+	setShowFeelings: (target: ShowFeeling) => void;
 }
 
-// submit form handler================
-const submitQuestionHandler = (e: FormEvent<HTMLFormElement>) : void => {
-    e.preventDefault();
-}
 
 const FeelingQuestions = (props: FeelingQProps) => {
-    const { setShowFeelings } = props;
+	const [answers, setAnswers] = useState({
+		favPerson: "",
+		favPlace: "",
+		favFood: "",
+		gratitude: "",
+		passion: "",
+		accomplishment: "",
+	});
+	const { setShowFeelings } = props;
+
+	const navigate = useNavigate();
+
+	const postAnswersToDB = async() => {
+		const res = await fetch('http://localhost:4000/favourite', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(answers)
+		});
+		const data = await res.json()
+		console.log(data);
+	}
+
+	// submit form handler================
+	const submitQuestionHandler = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		await postAnswersToDB();
+		navigate('/feeling');
+	};
+
+	const changeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+		const { name, value } = e.target;
+
+		setAnswers({ ...answers, [name]: value });
+	};
 
 	return (
 		<form className="question-form" onSubmit={submitQuestionHandler}>
-			{questions.map((q: questionType, index: number) => {
-				return (
-					<div className="fav-groups" key={index}>
-						<label htmlFor="fav-person">{q.question}</label>
-                        <input type="text" className="text-area" placeholder={q.label}/>
-					</div>
-				);
-			})}
-            <button type="submit" className="save-btn" onClick={() => setShowFeelings(false)}>Save</button>
+			<div className="fav-groups">
+				<label htmlFor="fav-person">
+					"Can you tell me about your favourite person? who do you enjoy hanging
+					out with?"
+				</label>
+				<input
+					type="text"
+					className="text-area"
+					name="favPerson"
+					placeholder="Sally"
+					value={answers.favPerson}
+					onChange={changeHandler}
+				/>
+			</div>
+			<div className="fav-groups">
+				<label htmlFor="fav-place">
+					where is your favourite place in your town
+				</label>
+				<input
+					type="text"
+					className="text-area"
+					placeholder="Tower bridge"
+					name="favPlace"
+					value={answers.favPlace}
+					onChange={changeHandler}
+				/>
+			</div>
+			<div className="fav-groups">
+				<label htmlFor="fav-person">What are you most grateful for?</label>
+				<input
+					type="text"
+					className="text-area"
+					placeholder="Family, friends, health,..."
+					name="gratitude"
+					value={answers.gratitude}
+					onChange={changeHandler}
+				/>
+			</div>
+
+			<div className="fav-groups">
+				<label htmlFor="fav-person">
+					What are the things you're most passionate about?
+				</label>
+				<input
+					type="text"
+					className="text-area"
+					placeholder="Animals, learn foreign languages"
+					name="passion"
+					value={answers.passion}
+					onChange={changeHandler}
+				/>
+			</div>
+			<div className="fav-groups">
+				<label htmlFor="fav-person">
+					What are your greatest accomplishments in life so far?
+				</label>
+				<input
+					type="text"
+					className="text-area"
+					placeholder="Graduating near the top of my class last year"
+					name="accomplishment"
+					value={answers.accomplishment}
+					onChange={changeHandler}
+				/>
+			</div>
+			<div className="fav-groups">
+				<label htmlFor="fav-person">Tell us about your favourite food?</label>
+				<input
+					type="text"
+					className="text-area"
+					placeholder="pizza"
+					name="favFood"
+					value={answers.favFood}
+					onChange={changeHandler}
+				/>
+			</div>
+
+			<button
+				type="submit"
+				className="save-btn"
+				onClick={() => setShowFeelings(false)}
+			>
+				Save
+			</button>
 		</form>
 	);
 };
-
-
 
 export default FeelingQuestions;
--- src/client/data/feelingQuestion.ts
+++ src/client/data/feelingQuestion.ts
@@ -1,34 +1,41 @@
 export interface questionType {
     question: string,
-    label: string
+    label: string,
+    name: string
 }
 
 export const questions:  Array<questionType> = [
     {
         question: "Can you tell me about your favourite person? who do you enjoy hanging out with?",
-        label: 'Sally'
+        label: 'Sally',
+        name: 'favPerson'
     },
     {
         question: "where is your favourite place in your town?",
-        label: 'Tower bridge'
+        label: 'Tower bridge',
+        name: 'favPlace'
     }
     ,
     {
         question: "What are you most grateful for?",
-        label: 'Family, friends, health,...'
+        label: 'Family, friends, health,...',
+        name: 'gratitude'
     }
     ,
     {
         question: "What are the things you're most passionate about?",
-        label: 'Animals, learn foreign languages'
+        label: 'Animals, learn foreign languages',
+        name: 'passion'
     }
     ,
     {
         question: "What are your greatest accomplishments in life so far?",
-        label: 'Graduating near the top of my class last year'
+        label: 'Graduating near the top of my class last year',
+        name: 'accomplishment'
     },
     {
         question:  "Tell us about your favourite food?",
-        label: 'pizza'
+        label: 'pizza',
+        name: 'favFood'
     }
 ]
