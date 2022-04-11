abb6e8f2c040ffbe29d1351a44aa035151ef1d58
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-11 18:24:53 +0100

    delete diary from goodmood page

--- src/client/pages/GoodMood.tsx
+++ src/client/pages/GoodMood.tsx
@@ -1,20 +1,19 @@
-import {useEffect, useState} from "react";
-import DiaryForm from "../components/DiaryForm";
+import { useEffect, useState } from "react";
 
-import '../styles/goodmood.css';
+import "../styles/goodmood.css";
 
 const GoodMood = () => {
-    const [quote, setQuote] = useState('');
+	const [quote, setQuote] = useState("");
 
-    useEffect(() => {
-        const fetchQuoteFromApi = async() => {
-            const quoteRes = await fetch('https://api.adviceslip.com/advice')
-            const quoteData = await quoteRes.json();
-            setQuote(quoteData.slip.advice)
-        }
-        fetchQuoteFromApi();
-    }, [])
-    
+	useEffect(() => {
+		const fetchQuoteFromApi = async () => {
+			const quoteRes = await fetch("https://api.adviceslip.com/advice");
+			const quoteData = await quoteRes.json();
+			setQuote(quoteData.slip.advice);
+		};
+		fetchQuoteFromApi();
+	}, []);
+
 	return (
 		<div className="mood-page">
 			<h1>Great, you're in a good mood!</h1>
@@ -22,20 +21,10 @@ const GoodMood = () => {
 				<h2>“{quote}”</h2>
 			</div>
 
-            <div className="diary-container">
-				<h3>Do you want to write about your feeling ? </h3>
-				<DiaryForm />
+			<div className="sharing-container">
+				<h3>You can share your feeling with others here:</h3>
+				<div className="sharing">Creating a forum In progress here...</div>
 			</div>
-
-            <div className="sharing-container">
-                <h3>You can share your feeling with others here:</h3>
-                {/* <div className="sharing">
-                    <div className="share">
-                        <img src="" alt="" />
-                        <p></p>
-                    </div>
-                </div> */}
-            </div>
 		</div>
 	);
 };
