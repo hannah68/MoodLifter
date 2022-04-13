4313c3617265463a26af6b327dbfeda6f3add2d5
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-13 17:19:34 +0100

    create profile saving functionality using state insted of db

--- src/client/pages/Profile.tsx
+++ src/client/pages/Profile.tsx
@@ -1,48 +1,99 @@
-import '../styles/profile.css';
-import {BsFillChatRightQuoteFill, BsFillJournalBookmarkFill} from 'react-icons/bs'
-import {GoDeviceCameraVideo} from 'react-icons/go'
-import {FaBook} from 'react-icons/fa'
+import { BsFillChatRightQuoteFill } from "react-icons/bs";
+import { GoDeviceCameraVideo } from "react-icons/go";
+import { FaBook } from "react-icons/fa";
 
-export interface IProfileProps {}
+import "../styles/profile.css";
 
-const Profile = () => {
+import { uniqueArrHandler } from "../utils/utils";
+
+import { RecommendationType } from "../interfaces";
+
+// profile props interface==================================
+export interface IProfileProps {
+	savedQuote: RecommendationType["quote"];
+	savedArticle: RecommendationType["article"];
+	savedVideo: RecommendationType["video"];
+}
+
+// profile component=======================================
+const Profile = (props: IProfileProps) => {
+	const { savedQuote, savedArticle, savedVideo } = props;
+
+	const checkUniqueQuote = (): RecommendationType["quote"] => {
+		return uniqueArrHandler(savedQuote);
+	};
+
+	const checkUniqueArticle = (): RecommendationType["article"] => {
+		return uniqueArrHandler(savedArticle);
+	};
+
+	const checkUniqueVideo = (): RecommendationType["video"] => {
+		return uniqueArrHandler(savedVideo);
+	};
+
 	return (
 		<div className="profile-page">
-      <div className="quote-save-container">
-        <h2>My Favourite quotes</h2>
-        <ul className='quote-list'>
-          <li><span><BsFillChatRightQuoteFill/></span>Life is a circle of happiness, sadness, hard times, and good times. If you are going through hard times, have faith that good times are on the way.</li>
-          <li><span><BsFillChatRightQuoteFill/></span>The more powerful and original a mind, the more it will incline towards the religion of solitude.</li>
-        </ul>
-      </div>
+			<div className="quote-save-container">
+				<h2>My Favourite quotes</h2>
+				<ul className="quote-list">
+					{savedQuote ? (
+						checkUniqueQuote().map((quote, index: number) => {
+							return (
+								<li key={index}>
+									<span>
+										<BsFillChatRightQuoteFill />
+									</span>
+									{quote.text}
+								</li>
+							);
+						})
+					) : (
+						<li>There is not any Favourite quote</li>
+					)}
+				</ul>
+			</div>
 			<div className="read-container">
-        <h2>Articles to read later</h2>
-        <ul className='article-save-list'>
-          <li>
-            <a href="/"><span><FaBook/></span>Four Ways Sadness May Be Good for You</a>
-          </li>
-          <li><a href="/"><span><FaBook/></span>Loneliness: Causes and Health Consequences</a></li>
-        </ul>
-      </div>
+				<h2>Articles to read later</h2>
+				<ul className="article-save-list">
+					{savedArticle ? (
+						checkUniqueArticle().map((article, index: number) => {
+							return (
+								<li key={index}>
+									<a href={article.link}>
+										<span>
+											<FaBook />
+										</span>
+										{article.title}
+									</a>
+								</li>
+							);
+						})
+					) : (
+						<p>There is not any article to read</p>
+					)}
+				</ul>
+			</div>
 			<div className="see-container">
-      <h2>Videos to watch later</h2>
-        <ul className='video-save-list'>
-          <li><a href="/"><span><GoDeviceCameraVideo/></span>6 Differences Between Sadness and Depression</a></li>
-          <li><a href="/"><span><GoDeviceCameraVideo/></span>How to cope with anxiety | Olivia Remes | TEDxUHasselt</a></li>
-        </ul>
-      </div>
-      <div className="diary-save-container">
-        <h2>My Diaries</h2>
-        <ul className='diary-list'>
-          <li>
-            <p><span><BsFillJournalBookmarkFill/></span>readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-          </li>
-          <li>
-            <p><span><BsFillJournalBookmarkFill/></span>0s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into </p>
-          </li>
-        </ul>
-      </div>
-			
+				<h2>Videos to watch later</h2>
+				<ul className="video-save-list">
+					{savedVideo ? (
+						checkUniqueVideo().map((video, id: number) => {
+							return (
+								<li key={id}>
+									<a href={video.link}>
+										<span>
+											<GoDeviceCameraVideo />
+										</span>
+										{video.title}
+									</a>
+								</li>
+							);
+						})
+					) : (
+						<p>There is not any video to watch</p>
+					)}
+				</ul>
+			</div>
 		</div>
 	);
 };
